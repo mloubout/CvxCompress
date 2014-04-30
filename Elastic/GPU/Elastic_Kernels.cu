8075863bd969bbcff37807b37f972410f4d13318
tjhc <tjhc@chevron.com>
2014-04-30 16:12:56 +0000

    d

--- Elastic/GPU/Elastic_Kernels.cu
+++ Elastic/GPU/Elastic_Kernels.cu
@@ -3,5 +3,5 @@
 #include "Earth_Model.cu"
 #include "Simple_Copy.cu"
 #include "Propagate_Particle_Velocities.cu"
-#include "Propagate_Stresses_Isotropic.cu"
+#include "Propagate_Stresses_Orthorhombic.cu"
 
---
+++ Elastic/GPU/gpuAssert.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+	if (code != cudaSuccess) 
+	{
+		fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
+
