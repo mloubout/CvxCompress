c5ff8ae574e9f4b1329458731dee47bec64b0eef
tjhc <tjhc@chevron.com>
2014-06-17 18:40:24 +0000

    Added elastic interpolation

---
+++ Elastic/GPU/Elastic_Interpolation.hxx
@@ -0,0 +1,12 @@
+#ifndef CVX_ESDRD_MI_TMJ_ELASTIC_INTERPOLATION_HXX
+#define CVX_ESDRD_MI_TMJ_ELASTIC_INTERPOLATION_HXX
+
+enum Elastic_Interpolation_t
+{
+	Point = 1,
+	Trilinear = 2,
+	Sinc = 3
+};
+
+#endif
+
