9a9982cd7309daed78b9029481c8c8f295c4a9e0
tjhc <tjhc@chevron.com>
2015-05-27 14:33:31 +0000

    Bump version number

--- Elastic/GPU/Elastic_Modeling_Job.hxx
+++ Elastic/GPU/Elastic_Modeling_Job.hxx
@@ -14,7 +14,7 @@ public:
 
 	static void Print_Version_Information()
 	{
-		printf("\nCVX 3D Elastic Orthorhombic Modeling - v1.15 - 05/14/15\n\n");
+		printf("\nCVX 3D Elastic Orthorhombic Modeling - v1.2 - 05/27/15\n\n");
 	}
 
 	bool Is_Valid() {return _Is_Valid;}
