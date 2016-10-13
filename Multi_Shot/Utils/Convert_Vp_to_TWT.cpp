3ffcf06aa4ee23ef3b730f24203f54672ae55047
tjhc <tjhc@chevron.com>
2016-10-13 15:01:19 +0000

    Add Convert_VP_to_TWT program

---
+++ Multi_Shot/Utils/Convert_Vp_to_TWT.cpp
@@ -0,0 +1,95 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void Print_Usage(const char* cmd)
+{
+	printf("\n");
+	printf("Usage : %s <svp-bin-file> <dz> <zwb>\n",cmd);
+	printf("<svp-bin-file> File containing the svp binary data.\n");
+	printf("<dz> Vertical cell size in meters.\n");
+	printf("<zwb> Water depth in meters.\n");
+	printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4)
+	{
+		Print_Usage(argv[0]);
+		return -1;
+	}
+
+	int ffid = 0;
+	if (sscanf(argv[1],"%d_Vp.bin",&ffid) < 1)
+	{
+		printf("Error! Unable to determine FFID from filename %s.\n",argv[1]);
+		Print_Usage(argv[0]);
+                return -8;
+	}
+
+	double dz = atof(argv[2]);
+	if (dz <= 0.0)
+	{
+		printf("Error! <dz> Must be positive number.\n");
+		Print_Usage(argv[0]);
+		return -2;
+	}
+		
+	double zwb = atof(argv[3]);
+	int iwb = (int)trunc(zwb/dz);
+	if (iwb <= 0)
+	{
+		printf("Error! <zwb> Must be positive number >= <dz>.\n");
+		Print_Usage(argv[0]);
+		return -3;
+	}
+	
+	struct stat st;
+	if (stat(argv[1],&st) != 0)
+	{
+		printf("Error! Unable to stat file %s.\n",argv[1]);
+		Print_Usage(argv[0]);
+		return -4;
+	}
+	int nsamp = st.st_size / sizeof(float);
+	if (nsamp <= 0)
+	{
+		printf("Error! File %s is empty.\n",argv[1]);
+		Print_Usage(argv[0]);
+                return -5;
+	}
+	if (iwb >= nsamp)
+	{
+		printf("Error! <zwb> is beyond depth of <svp-bin-file> %s.\n",argv[1]);
+		Print_Usage(argv[0]);
+                return -6;
+	}
+	
+	float* samples = new float[nsamp];
+	FILE* fp = fopen(argv[1],"rb");
+	int nread = fread((void*)samples,sizeof(float),nsamp,fp);
+	fclose(fp);
+	if (nread < nsamp)
+	{
+		printf("Error! Unable to read all %d values from <svp-bin-file> %s.\n",nsamp,argv[1]);
+		Print_Usage(argv[0]);
+                return -7;
+	}
+
+	double owt = 0.0;
+	for (int i = 0;  i < iwb;  ++i)
+	{
+		owt = owt + (dz / samples[i]);
+	}
+	double avg_Vp = ((double)iwb * dz) / owt;
+	double twt = 2.0 * zwb / avg_Vp;
+
+	printf("%d %.2f %.2f\n",ffid,zwb,twt*1e3);
+
+	delete [] samples;
+	return 0;
+}
