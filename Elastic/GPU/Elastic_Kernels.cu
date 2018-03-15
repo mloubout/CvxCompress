017824bf17c8d3450c301c2750bc6cf6ba98195c
samtkaplan <sam.kaplan@chevron.com>
2018-03-15 13:35:30 -0500

    just CvxCompress, please

--- Elastic/GPU/Elastic_Kernels.cu
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-#include "gpuAssert.h"
-#include "Compute_ABC.cu"
-#include "Earth_Model.cu"
-#include "Simple_Copy.cu"
-#include "Propagate_Particle_Velocities.cu"
-#include "Propagate_Stresses_Orthorhombic.cu"
-#include "Extract_Receiver_Values.cu"
-
